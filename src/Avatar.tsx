8f2f6329263f4bb72508dcf852ceae086b62315d
Hexiang Qi <qihexiang@outlook.com>
2021-09-17 09:11:26 +0800

    Avatar border shadow

--- src/Avatar.tsx
+++ src/Avatar.tsx
@@ -3,7 +3,9 @@ import React from "react";
 export default function Avatar(props: { imageUrl: string }) {
     return <div style={{ 
         margin: "auto",
-        width: "80%", 
+        width: "60%",
+        borderRadius: "50%",
+        boxShadow: "0 0 16px silver",
         aspectRatio: "1/1", 
         backgroundImage: `url(${props.imageUrl})`, 
         backgroundRepeat: "no-repeat", backgroundPosition: "center", backgroundSize: "cover" 
