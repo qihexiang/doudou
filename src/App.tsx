ecab8ac76d0fdf341b132c0dab1742990870ad4a
Hexiang <hexiang@polarbird.lan>
2023-11-06 22:18:06 +0800

    update 231017

--- src/App.tsx
+++ src/App.tsx
@@ -10,11 +10,14 @@ function App() {
   return (
     <div className="App" style={{display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", backgroundImage: `url(/doudou/cherryblossom.jpg)`, backgroundPosition: "center", backgroundRepeat: "no-repeat", backgroundSize: "cover", ...fullScreen}}>
       <Container style={{
-        borderTop: "16px solid #ec407a", borderBottom: "16px solid #ec407a", boxShadow: "0 0 16px grey", borderRadius: 4, backgroundImage: "url(/doudou/otter.jpg)", backgroundRepeat: "no-repeat", backgroundPosition: "right bottom", backgroundSize: "44px 44px"
+        position: "relative",
+        borderTop: "16px solid #ec407a", borderBottom: "16px solid #ec407a", boxShadow: "0 0 16px grey", borderRadius: 4,
       }}>
         <Avatar imageUrl="/doudou/goose.png" />
         <Days now={now} />
         <Anniversary now={now} />
+        <img src="/otter.jpg" width={44} height={44} style={{objectFit: "contain", position: "absolute", left: 0, bottom: 0}}></img>
+        <img src="/collaboration.gif" width={44} height={44} style={{objectFit: "contain", position: "absolute", right: 0, bottom: 0}}></img>
       </Container>
     </div>
   )
